4161ca05f9f515f7141eaff94f25711f4429b9ec
Dilip Upadhyay <dilip.upadhyay@pfizer.com>
2025-01-01 02:58:29 +0530

    moved the db property to class file

---
+++ src/main/java/com/webflick/configurations/DataSourceConfig.java
@@ -0,0 +1,24 @@
+package com.webflick.configurations;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+@Configuration
+public class DataSourceConfig {
+
+    @Primary
+    @Bean
+    public DataSource getDataSource() {
+        DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:mem:testdb");
+        dataSourceBuilder.username("SA");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
+    }
+}
